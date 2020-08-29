e9a87eb5504df7e9735412615922b7d359b3cac2
hassanamin994 <hassanmamin994@gmail.com>
2020-08-29 19:59:18 +0200

    remove deps

--- services/index.js
+++ services/index.js
@@ -1,12 +1,5 @@
-const {
-    STORAGE_SERVICE_API_ROOT,
-    TEXT_TO_SPEECH_SERVICE_API_ROOT,
-} = process.env;
-
-
-
-const storageService = require('@videowiki/services/storage')(STORAGE_SERVICE_API_ROOT);
-const textToSpeechService = require('@videowiki/services/textToSpeach')(TEXT_TO_SPEECH_SERVICE_API_ROOT);
+const storageService = require('@videowiki/vendors/storage');
+const textToSpeechService = require('@videowiki/vendors/textToSpeech') 
 
 module.exports = {
     storageService,
